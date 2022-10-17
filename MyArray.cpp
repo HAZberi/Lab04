c81e0a967c1ab502b1933bed99966c808d73224a
Hassaan Zuberi <mhazuberi@gmail.com>
2022-10-17 14:17:55 -0600

    updated MyArray

--- MyArray.cpp
+++ MyArray.cpp
@@ -1,63 +1,77 @@
 /*
  *  MyArray.cpp
  *  ENSF 614 Lab 4, Exercise A
+ *  Class implemenatation for MyArray.
  *  Completed by: Hassaan Ahmed Zuberi
  *  Submitted on: Oct 17, 2022
  */
 
-#include "MyArray.h" 
+#include "MyArray.h"
 
-MyArray::MyArray(){
+MyArray::MyArray()
+{
     sizeM = 0;
     storageM = new EType[sizeM];
 }
 
-MyArray::MyArray(const EType *builtin, int sizeA){
+MyArray::MyArray(const EType *builtin, int sizeA)
+{
     sizeM = sizeA;
     storageM = new EType[sizeM];
-    for(int i=0; i<sizeM; i++){
+    for (int i = 0; i < sizeM; i++)
+    {
         storageM[i] = *(builtin + i);
     }
 }
 
-MyArray::MyArray(const MyArray& source){
+MyArray::MyArray(const MyArray &source)
+{
     sizeM = source.size();
     storageM = new EType[sizeM];
-    for(int i=0; i<sizeM; i++){
+    for (int i = 0; i < sizeM; i++)
+    {
         storageM[i] = source.at(i);
     }
 }
 
-EType MyArray::at(int i) const{
+EType MyArray::at(int i) const
+{
     return storageM[i];
 }
 
-int MyArray::size() const{
+int MyArray::size() const
+{
     return sizeM;
 }
 
-void MyArray::set(int i, EType new_value){
+void MyArray::set(int i, EType new_value)
+{
     storageM[i] = new_value;
 }
 
-void MyArray::resize(int new_size){
+void MyArray::resize(int new_size)
+{
     EType *temp = new EType[new_size];
 
-    for(int i=0; i<new_size; i++){
+    for (int i = 0; i < new_size; i++)
+    {
         temp[i] = *(storageM + i);
     }
     sizeM = new_size;
-    
-    delete [] storageM;
+
+    delete[] storageM;
     storageM = temp;
 }
 
-MyArray& MyArray::operator=(const MyArray &rhs){
-    if(this != &rhs){
-        delete [] storageM;
+MyArray &MyArray::operator=(const MyArray &rhs)
+{
+    if (this != &rhs)
+    {
+        delete[] storageM;
         sizeM = rhs.size();
         storageM = new EType[sizeM];
-        for(int i=0; i<sizeM; i++){
+        for (int i = 0; i < sizeM; i++)
+        {
             storageM[i] = rhs.at(i);
         }
     }
@@ -65,6 +79,7 @@ MyArray& MyArray::operator=(const MyArray &rhs){
     return *this;
 }
 
-MyArray::~MyArray() {
-    delete [] storageM;
+MyArray::~MyArray()
+{
+    delete[] storageM;
 }
